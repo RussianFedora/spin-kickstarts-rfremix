8b8d3b4193f8e6f675f7208cd5c28b94cce6e452
Tigro <ashejn@gmail.com>
2015-11-17 18:10:47 +0300

    update git url

--- get_sources.sh
+++ get_sources.sh
@@ -2,6 +2,6 @@
 NR=$(rpm -q --specfile *.spec --qf "%{name}-%{version}")
 
 rm -rf $NR
-git clone -q -b f23/master --recursive git://github.com/Tigro/spin-kickstarts-rfremix.git $NR
+git clone -q -b f23/master --recursive git://github.com/RussianFedora/rfremix-kickstarts.git $NR
 tar cjf $NR.tar.bz2 $NR --exclude=.git
 rm -rf $NR
