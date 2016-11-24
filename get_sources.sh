9c15636e5628d9197221a127f663b172be4f0817
Tigro <ashejn@gmail.com>
2016-11-25 00:20:07 +0300

    update tar options

--- get_sources.sh
+++ get_sources.sh
@@ -2,6 +2,6 @@
 NR=$(rpm -q --specfile *.spec --qf "%{name}-%{version}")
 
 rm -rf $NR
-git clone -q -b f24/master --recursive git://github.com/RussianFedora/rfremix-kickstarts.git $NR
-tar cjf $NR.tar.bz2 $NR --exclude=.git
+git clone -q -b f25/master --recursive git://github.com/RussianFedora/rfremix-kickstarts.git $NR
+tar -cva --exclude-vcs --exclude-vcs-ignores -f $NR.tar.bz2 $NR --exclude=.git
 rm -rf $NR
